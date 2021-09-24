75d349f40fbe4d5bfb2bae4aab4b70e965db4975
MM-Uyghur <mm.well@outlook.com>
2021-09-23 23:34:52 -0400

    AddNewBook task done

--- src/test/java/LibraryFrontEndAutomation/LibraryOne4AllF/Library_3Functionality_5Tests.java
+++ src/test/java/LibraryFrontEndAutomation/LibraryOne4AllF/Library_3Functionality_5Tests.java
@@ -1,10 +1,12 @@
 package LibraryFrontEndAutomation.LibraryOne4AllF;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import utilities.Driver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -114,11 +116,11 @@ public class Library_3Functionality_5Tests extends Driver {
 
         driver.findElement(By.xpath("//span//i[@class='fa fa-plus']")).click(); // add user button
 
-        driver.findElement(By.name("full_name")).sendKeys("Dickzy Dar");
+        driver.findElement(By.name("full_name")).sendKeys("Zeck Dar");
 
-        driver.findElement(By.name("password")).sendKeys("DDD" + i);
+        driver.findElement(By.name("password")).sendKeys("ZDD" + i);
 
-        driver.findElement(By.name("email")).sendKeys("DDDD" + i + "@gmail.com");
+        driver.findElement(By.name("email")).sendKeys("ZDDD" + i + "@gmail.com");
 
         driver.findElement(By.xpath("//select[@id='user_group_id']")).click();//User Group
 
@@ -144,7 +146,7 @@ public class Library_3Functionality_5Tests extends Driver {
         WebElement verifyName = driver.findElement(By.xpath("//tr[1]//td[3]")); //verify user name
         WebElement verifyEmail = driver.findElement(By.xpath("//tr[1]/td[4]"));//verify suer email
 
-        if (verifyName.equals("Dickzy Dar") && verifyEmail.equals("DDDD" + i + "@gmail.com")) {
+        if (verifyName.equals("Zeck Dar") && verifyEmail.equals("ZDDD" + i + "@gmail.com")) {
             System.out.println("New user added successfully and verified !");
         } else {
             System.out.println("New user was not found, verification failed !!");
@@ -152,6 +154,70 @@ public class Library_3Functionality_5Tests extends Driver {
 
 
         i++;
+    }
+
+    @Test
+    public void TestNewBook() throws InterruptedException {
+
+        driver.get("http://library2.cybertekschool.com/login.html");
+        //enter email and password:
+        WebElement LogInBox = driver.findElement(By.id("inputEmail"));
+        LogInBox.sendKeys("librarian14@library");
+
+        WebElement PassBox = driver.findElement(By.id("inputPassword"));
+        PassBox.sendKeys("Sdet2022*");
+
+        WebElement SignInBtn = driver.findElement(By.tagName("button"));
+        SignInBtn.submit();
+        System.out.println("LoggedIn");
+
+
+        WebElement Books = driver.findElement(By.xpath("//span[.='Books']"));
+        System.out.println("Books.getText() = " + Books.getText());
+        Books.click();
+
+        WebElement addBook = driver.findElement(By.xpath("//section[@id=\"books\"]//span/a"));
+        addBook.click();
+
+        WebElement Name = driver.findElement(By.name("name"));
+        Name.sendKeys("History in the making");
+
+
+        WebElement ISBN = driver.findElement(By.name("isbn"));
+        ISBN.sendKeys("222111000");
+
+        WebElement Year = driver.findElement(By.name("year"));
+        Year.sendKeys("2022");
+
+        WebElement author = driver.findElement(By.xpath("//input[@name='author']"));
+        author.sendKeys("Alax Mohan");
+
+        WebElement Category = driver.findElement(By.id("book_group_id"));
+
+        Select selectBook = new Select(Category);
+        selectBook.selectByVisibleText("Essay");
+        //selectBook.selectByIndex();
+        //selectBook.selectByValue("18");
+
+        WebElement description = driver.findElement(By.xpath("//textarea"));
+        description.sendKeys("History of 21st century");
+
+
+        WebElement saveChangesBtn = driver.findElement(By.xpath("//button[@type='submit']"));
+        saveChangesBtn.submit();
+
+        Thread.sleep(2000);
+        WebElement search = driver.findElement(By.tagName("input")); //className==form-control input-sm input-small input-inline
+        search.sendKeys("Alax");
+
+        Thread.sleep(5000);
+        WebElement verified = driver.findElement(By.xpath("//tr[1]//td[.='Alax Mohan']"));
+        Assertions.assertEquals("Alax Mohan", verified.getText());
+        System.out.println("New Book author: verified.getText() = " + verified.getText());
+        System.out.println("new book verified");
+
+        Thread.sleep(3000);
+
 
 
     }
